c144f12de1dd2daa4cd1336f30ccfb46ca7ba3c7
Alexandre HERGAUX <alexandre.hergaux@icloud.com>
2026-06-30 21:48:30 +0200

    bug fetch mail fix

--- mail.py
+++ mail.py
@@ -47,9 +47,9 @@ class iCloudConnection:
         log.debug(f"Fetching {count} messages from mailbox")
         messages = []
         for i in range(1, count + 1):
-            msg = self.imap.FetchSingle(i, True)
+            msg = self.imap.FetchSingle(i, False)
             if msg is None:
-                log.error(f"Failed to fetch message at index {i}")
+                log.error(f"Failed to fetch message at index {i}: {self.imap.lastErrorText()}")
             else:
                 log.debug(f"Fetched message {i}: {getattr(msg, 'header', '(no header)')!r:.80}")
                 messages.append(msg)
